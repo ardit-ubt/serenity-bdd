c8c30f3852db01230caa128bb0e4d66ed6852183
Fitim63 <fm38739@ubt-uni.net>
2020-04-22 22:47:28 +0200

    first_commit

--- src/test/java/definitions/Hooks.java
+++ src/test/java/definitions/Hooks.java
@@ -12,6 +12,7 @@ public class Hooks {
 
     @Before
     public void setup() {
+        System.getProperties("webdriver.gecko.drivers", "C:\\Users\\fitim\\serenity-bdd\\drivers\\geckodriver.exe")
         getWebdriverManager().getWebdriver().manage().window().maximize();
         getWebdriverManager().getWebdriver().get("https://facebook.com");
     }
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -1,5 +1,6 @@
 Feature: Login
 
+  @Debug
   Scenario: Login with valid credentials
     Given User is logged in
     Then User should see Dashboard
